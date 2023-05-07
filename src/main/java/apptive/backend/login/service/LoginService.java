5047e321783e77df1f52789cb45fb1ad3444410b
seasameoil <whdwlgus12@gmail.com>
2023-05-07 17:05:24 +0900

    로그인 구현

--- src/main/java/apptive/backend/login/service/LoginService.java
+++ src/main/java/apptive/backend/login/service/LoginService.java
@@ -1,19 +1,12 @@
 package apptive.backend.login.service;
 
-import apptive.backend.exception.ExceptionEnum;
-import apptive.backend.exception.login.LoginException;
-import apptive.backend.exception.login.MemberNotExistException;
-import apptive.backend.login.domain.Member;
 import apptive.backend.login.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Slf4j
 @Service
