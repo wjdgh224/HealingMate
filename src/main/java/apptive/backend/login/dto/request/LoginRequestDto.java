c7354988fa1bc382498b39b79a7bcc9c865f684c
seasameoil <whdwlgus12@gmail.com>
2023-05-08 21:28:08 +0900

    logout 완성

--- src/main/java/apptive/backend/login/dto/request/LoginRequestDto.java
+++ src/main/java/apptive/backend/login/dto/request/LoginRequestDto.java
@@ -1,14 +1,30 @@
 package apptive.backend.login.dto.request;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Getter
 public class LoginRequestDto {
 
-    @NotNull
-    private String memberNickname;
+    @Getter
+    public static class LoginDto {
+        @NotNull
+        private String memberNickname;
 
-    @NotNull
-    private String password;
+        @NotNull
+        private String password;
+    }
+
+    @Getter
+    public static class TokenDto {
+
+        private String accessToken;
+        private String refreshToken;
+
+        public TokenDto(String accessToken, String refreshToken) {
+            this.accessToken = accessToken;
+            this.refreshToken = refreshToken;
+        }
+    }
 }
